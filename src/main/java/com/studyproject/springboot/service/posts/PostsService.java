c8637d853bcf8e04a4490f75e3ffcc6eeb3ad6d0
great-park <cksgh1735@naver.com>
2022-09-10 23:26:27 +0900

    수정중

---
+++ src/main/java/com/studyproject/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.studyproject.springboot.service.posts;
+
+import com.studyproject.springboot.domain.posts.PostsRepository;
+import com.studyproject.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/studyproject/springboot/web/PostApiController.java
@@ -0,0 +1,17 @@
+package com.studyproject.springboot.web;
+
+import com.studyproject.springboot.service.posts.PostsService;
+import com.studyproject.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
